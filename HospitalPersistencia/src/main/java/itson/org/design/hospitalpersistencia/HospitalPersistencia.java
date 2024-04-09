4ac6d3d578e13888a78d95f6e40d5fdb4a23bfaf
Puert4 <jullian.puerta248239@potros.itson.edu.mx>
2024-04-09 10:49:41 -0700

    Se agregaron los proyectosaa

---
+++ HospitalPersistencia/src/main/java/itson/org/design/hospitalpersistencia/HospitalPersistencia.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package itson.org.design.hospitalpersistencia;
+
+/**
+ *
+ * @author Laboratorios
+ */
+public class HospitalPersistencia {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
